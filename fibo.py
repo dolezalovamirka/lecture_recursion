6ff2615e9c1aa6258941e48a95db9e57c3d1d0e2
Miroslava Dolezalova <mirka.dolezalova7@seznam.cz>
2025-04-28 09:48:01 +0200

    hotova funkce

--- fibo.py
+++ fibo.py
@@ -1,6 +1,20 @@
+def recursive_nth_fibo(number):
+    if number == 0:
+        return 0
+    elif number == 1:
+        return 1
+    elif number >= 1:
+        return recursive_nth_fibo(number - 1) + recursive_nth_fibo(number - 2)
+
 
 def main():
-    pass
+    vysledek = recursive_nth_fibo(6)
+    print(vysledek)
+    n = int(input("Zadejte kladne cislo: "))
+    fibonacciho_list = []
+    for i in range(n):
+        fibonacciho_list.append(recursive_nth_fibo(i))
+    print(fibonacciho_list)
 
 
 if __name__ == "__main__":
